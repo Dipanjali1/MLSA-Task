5e5ea3ca7d57c262bf133f9afbfd2097360276ce
Dipanjali Gubreley <dipanjali180302@gmail.com>
2023-09-03 18:24:57 +0530

    Add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Dipanjali Gubreley",
+  title: "Web Designer & Software Developer",
+  email: "Dipanjali.Gubreley@studentambassadors.com",
+  gitHub: "Dipanjali1",
+  instagram: "",
+  linkedIn: "Dipanjali",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "Dipanjali180302",
+  youTube: "Dipanjali Gubreley",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,26 +16,26 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/blossoms.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "plum blossoms background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm an aspiring software development engineer, currently pursuing B.Tech in IT from KIET Group of Institutions";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "Web developer",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "Interactive design",
+  "System Designer",
+  "User interfaces",
   "Graphic design",
 ];
 
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I'm a problem solver at heart, with a deep interest in technology and an insatiable appetite for learning new things.";
 
 const About = () => {
   return (
